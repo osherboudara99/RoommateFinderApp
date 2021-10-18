1437690e6a4ee989e57e92c37e3be0ef8d3a0932
Cyrus Tabatabai <cyrustaba@hotmail.com>
2021-10-18 16:33:01 -0700

    Add working splash screen and logo assets

--- App.js
+++ App.js
@@ -11,22 +11,23 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
 
-  export default function App() {
+export default function App() {
 
-    const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
 
-    return (
-      <NavigationContainer>
+  return (
+    <NavigationContainer>
       <Stack.Navigator>
-        
+
         {isLoggedIn == false ? (
-         <Stack.Screen name="LoginNavigation" component={LoginNavigation} options={{ headerShown: false,
-         }} initialParams={{ setIsLoggedIn: setIsLoggedIn }}/>
-       ) : (
-           <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} initialParams={{ setIsLoggedIn: setIsLoggedIn }}/>)}
-    
+          <Stack.Screen name="LoginNavigation" component={LoginNavigation} options={{
+            headerShown: false,
+          }} initialParams={{ setIsLoggedIn: setIsLoggedIn }} />
+        ) : (
+          <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} initialParams={{ setIsLoggedIn: setIsLoggedIn }} />)}
+
 
       </Stack.Navigator>
     </NavigationContainer>
-    );
+  );
 }
---
+++ src/pages/SplashScreen.js
@@ -0,0 +1,106 @@
+import { AirlineSeatReclineNormalOutlined } from "@material-ui/icons";
+import React, { useEffect, useState, useRef } from "react";
+import { Text, View, StyleSheet, Animated } from "react-native";
+import RNBootSplash from "react-native-bootsplash";
+
+let bootSplashLogo = require('../../assets/logo.png');
+
+let fakeApiCallWithoutBadNetwork = (ms) =>
+    new Promise((resolve) => setTimeout(resolve, ms));
+
+let SplashScreen = () => {
+    let [bootSplashIsVisible, setBootSplashIsVisible] = useState(true);
+    let [bootSplashLogoIsLoaded, setBootSplashLogoIsLoaded] = useState(false);
+    let opacity = useRef(new Animated.Value(1));
+    let translateY = useRef(new Animated.Value(0));
+
+    let init = async () => {
+        // You can uncomment this line to add a delay on app startup
+        await fakeApiCallWithoutBadNetwork(3000);
+
+        try {
+            await RNBootSplash.hide();
+
+            Animated.stagger(250, [
+                Animated.spring(translateY.current, {
+                    useNativeDriver: true,
+                    toValue: -50,
+                }),
+                Animated.spring(translateY.current, {
+                    useNativeDriver: true,
+                    toValue: Dimensions.get("window").height,
+                }),
+            ]).start();
+
+            Animated.timing(opacity.current, {
+                useNativeDriver: true,
+                toValue: 0,
+                duration: 150,
+                delay: 350,
+            }).start(() => {
+                setBootSplashIsVisible(false);
+            });
+        } catch (error) {
+            setBootSplashIsVisible(false);
+        }
+    };
+
+    useEffect(() => {
+        bootSplashLogoIsLoaded && init();
+    }, [bootSplashLogoIsLoaded]);
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Welcome to Roommate Finder!</Text>
+
+            {bootSplashIsVisible && (
+                <Animated.View
+                    style={[
+                        StyleSheet.absoluteFill,
+                        styles.bootsplash,
+                        { opacity: opacity.current },
+                    ]}
+                >
+                    <Animated.Image
+                        source={bootSplashLogo}
+                        fadeDuration={0}
+                        onLoadEnd={() => setBootSplashLogoIsLoaded(true)}
+                        style={[
+                            styles.logo,
+                            { transform: [{ translateY: translateY.current }] },
+                        ]}
+                    />
+                </Animated.View>
+            )}
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#F5FCFF",
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: "700",
+        margin: 20,
+        lineHeight: 30,
+        color: "#333",
+        textAlign: "center",
+    },
+    bootsplash: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#F5FCFF",
+    },
+    logo: {
+        height: 200,
+        width: 900,
+    },
+});
+
+export default SplashScreen;
