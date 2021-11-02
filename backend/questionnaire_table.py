523a967e20e063fac50e1b0f9419fdda04d8484c
vs256 <44126680+vs256@users.noreply.github.com>
2021-11-02 13:45:28 -0700

    changes to table

--- backend/questionnaire_table.py
+++ backend/questionnaire_table.py
@@ -15,17 +15,17 @@ def questionaire_table_creation():
 
         cursor = cnx.cursor()
         #still needs editing
-        cursor.execute("CREATE TABLE IF NOT EXISTS SeniorProject.questionaire ( \
-  `questionaire_id` int(10) unsigned NOT NULL AUTO_INCREMENT, \
-  `userid` int(10) DEFAULT NULL, \
-  `zipcode` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL, \
-  `phone` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL, \
-  `password` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL, \
-  `email` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL, \
-  `birthDay` DATE DEFAULT NULL,\
-  PRIMARY KEY (`questionaire_id`), \
-  CONSTRAINT `qs_user_id` FOREIGN KEY (`userid`) REFERENCES `SeniorProject.Users` (`userid`)\
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci")
+#         cursor.execute("CREATE TABLE IF NOT EXISTS SeniorProject.questionaire ( \
+#   `questionaire_id` int(10) unsigned NOT NULL AUTO_INCREMENT, \
+#   `userid` int(10) DEFAULT NULL, \
+#   `zipcode` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL, \
+#   `phone` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL, \
+#   `password` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL, \
+#   `email` varchar(45) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL, \
+#   `birthDay` DATE DEFAULT NULL,\
+#   PRIMARY KEY (`questionaire_id`), \
+#   CONSTRAINT `qs_user_id` FOREIGN KEY (`userid`) REFERENCES `SeniorProject.Users` (`userid`)\
+# ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci")
     
         cursor.close()
         cnx.close()
