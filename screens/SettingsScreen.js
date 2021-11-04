d94cdedb497543c5fba2a80755b70c51e17e39e4
vs256 <44126680+vs256@users.noreply.github.com>
2021-11-03 22:51:27 -0700

    Added home screen

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,18 +1,22 @@
 import * as React from "react";
 import { Text, View, Button } from "react-native";
 
-function SettingsScreen() {
+function SettingsScreen({navigation, route}) {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#009387",
-      }}
-    >
-      <Text color="white">Settings!</Text>
-    </View>
+    style={{
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#009387",
+    }}
+  >
+    <Button
+      title="Log me out"
+      onPress={() => route.params.setIsLoggedIn(false)}
+    />
+    <Text color="white">Settings!</Text>
+  </View>
   );
 }
 
---
+++ src/consts/houses.js
@@ -0,0 +1,52 @@
+const houses = [
+  {
+    id: '1',
+    title: 'LF 2-3 roommate in 4 bedroom house',
+    location: 'East Side Los Angeles',
+    image: require('../assets/house1.jpg'),
+    details: `This building is located in the Oliver area, withing walking distance of shops...`,
+    interiors: [
+      require('../assets/interior1.jpg'),
+      require('../assets/interior2.jpg'),
+      require('../assets/interior3.jpg'),
+    ],
+  },
+  {
+    id: '2',
+    title: 'Private room in house 1 person',
+    location: 'Down town house suite Los Angeles',
+    image: require('../assets/house2.jpg'),
+    details: `This building is located in the Oliver area, withing walking distance of shops...`,
+    interiors: [
+      require('../assets/interior1.jpg'),
+      require('../assets/interior2.jpg'),
+      require('../assets/interior3.jpg'),
+    ],
+  },
+  {
+    id: '3',
+    title: 'Entire bedroom/bath combo',
+    location: '3Mins to Skytrain/Garden/Stadium/100% California',
+    image: require('../assets/house3.jpg'),
+    details: `This building is located in the Oliver area, withing walking distance of shops...`,
+    interiors: [
+      require('../assets/interior1.jpg'),
+      require('../assets/interior2.jpg'),
+      require('../assets/interior3.jpg'),
+    ],
+  },
+  {
+    id: '4',
+    title: 'Private room in apartment',
+    location: 'Small room in cozy DTLA apartment! California',
+    image: require('../assets/house4.jpg'),
+    details: `This building is located in the Oliver area, withing walking distance of shops...`,
+    interiors: [
+      require('../assets/interior1.jpg'),
+      require('../assets/interior2.jpg'),
+      require('../assets/interior3.jpg'),
+    ],
+  },
+];
+
+export default houses;
