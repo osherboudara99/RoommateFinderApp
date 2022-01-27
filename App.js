2a437daa3b2fb8d988c6288c75028b7bf5437605
vs256 <vs256@gmail.com>
2022-01-27 13:39:02 -0800

    functionality

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import LoginNavigation from "./navigation/LoginNavigation";
 import AppNavigation from "./navigation/AppNavigation";
-import ProfileScreen from "./screens/ProfileScreen";
+import Questionnaire from "./screens/Questionnaire";
 
 import React, { useState } from "react";
 
@@ -13,30 +13,5 @@ export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [shownSplashScreen, setShownSplashScreen] = useState(false);
 
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        {isLoggedIn == false ? (
-          <Stack.Screen
-            name="LoginNavigation"
-            component={LoginNavigation}
-            options={{ headerShown: false }}
-            initialParams={{
-              setIsLoggedIn: setIsLoggedIn,
-              shownSplashScreen: shownSplashScreen,
-              setShownSplashScreen: setShownSplashScreen,
-
-            }}
-          />
-        ) : (
-          <Stack.Screen
-            name="AppNavigation"
-            component={AppNavigation}
-            options={{ headerShown: false }}
-            initialParams={{ setIsLoggedIn: setIsLoggedIn }}
-          />
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <Questionnaire />;
 }
